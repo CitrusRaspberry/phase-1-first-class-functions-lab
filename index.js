ed3891388c39a555eca18a447686d0f632d669f4
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-03 11:30:13 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers() {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers() {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return (fare) => fare * multiplier;
+}
+
+function fareDoubler(fare) {
+  const double = createFareMultiplier(2);
+  return double(fare);
+}
+
+function fareTripler(fare) {
+  const triple = createFareMultiplier(3);
+  return triple(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+  return func(arrayOfDrivers);
+}
